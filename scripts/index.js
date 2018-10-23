9b1b96eebbda59f5f50d823d83d07e43bbee5d50
Steven Taesung Kim <steventaesungkim@gmail.com>
2018-10-23 17:54:49 -0400

    created the skeleton for the INFO column and activated the button

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,37 @@
 // ===========================
-// DOM selector
+// Global Cache
 // ===========================
+
+
+// ===========================
+// Global Variable
+// ===========================
+
+
+// ===========================
+// Constants
+// ===========================
+
+
+// ===========================
+// DOM selection
+// ===========================
+const buttonElement = document.querySelector('[data-button]');
 
 // ===========================
 // Helper functions
 // ===========================
-
-
-
-
+function initiate(){
+    console.log("Button is good, let's begin");
+}
 
 
 
 // ===========================
-// 
+// Main event Listener
 // ===========================
+function main(){
+    buttonElement.addEventListener('click', initiate) //initiate will be renamed according to the function that we want to call first
+};
+
+main();
